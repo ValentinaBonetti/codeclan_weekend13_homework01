05f424fc0dc9b9bb83b3113d7ec295b87a1bd162
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-11-30 14:58:46 +0000

    Object deconstructing example in  instrument_family_view.js

--- client/src/views/instrument_family_view.js
+++ client/src/views/instrument_family_view.js
@@ -1,5 +1,10 @@
 import PubSub from '../helpers/pub_sub.js';
 
+// In instrument_family_view.js refactor the render method to destructure the
+// instrument family's properties as the family is passed into the function.
+// This will mean you can refer to the just the properties inside the function.
+// For example, name, rather than family.name.
+
 class InstrumentFamilyView {
   constructor(container) {
     this.container = container;
@@ -13,18 +18,25 @@ class InstrumentFamilyView {
   };
 
   render(family) {
+
+    // DECONSTRUCTOR EXAMPLE:
+    // var o = {p: 42, q: true};
+    // var {p, q} = o
+    // p and q are now directly accessible.
+    
+    var {name,description,instruments} = family;
     this.container.innerHTML = '';
 
-    const familyName = this.createElement('h2', family.name);
+    const familyName = this.createElement('h2', name);
     this.container.appendChild(familyName);
 
-    const familyDescription = this.createElement('p', family.description);
+    const familyDescription = this.createElement('p', description);
     this.container.appendChild(familyDescription);
 
     const instrumentListTitle = this.createElement('h3', 'Instruments include:');
     this.container.appendChild(instrumentListTitle);
 
-    const instrumentList = this.createInstrumentList(family.instruments);
+    const instrumentList = this.createInstrumentList(instruments);
     this.container.appendChild(instrumentList);
   };
 
