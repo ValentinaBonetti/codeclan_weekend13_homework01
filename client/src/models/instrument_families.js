118edc46db5566b9b71bf3be106e3806f95a0385
Valentina Bonetti <valentina.bonetti@strath.ac.uk>
2018-11-30 14:49:50 +0000

    Trasform mod.export->require into default export->import

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -1,4 +1,4 @@
-const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
 class InstrumentFamilies  {
   constructor(data) {
@@ -21,4 +21,4 @@ class InstrumentFamilies  {
 
 };
 
-module.exports = InstrumentFamilies;
+export default InstrumentFamilies;
